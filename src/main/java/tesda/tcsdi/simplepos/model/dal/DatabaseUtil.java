c130a545c45e0c9088c8cf47effbee8b4b6a60ab
ed realuyo <ed.realuyo@gmail.com>
2024-11-26 02:32:28 +0800

    Simplified ProductDB.java by making a general query method to DatabaseUtil.java

--- src/main/java/tesda/tcsdi/simplepos/model/dal/DatabaseUtil.java
+++ src/main/java/tesda/tcsdi/simplepos/model/dal/DatabaseUtil.java
@@ -1,8 +1,6 @@
 package tesda.tcsdi.simplepos.model.dal;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseUtil {
     private static String hostName = "localhost";
@@ -10,13 +8,9 @@ public class DatabaseUtil {
     private static String user = "root";
     private static String password = "MySQLpassword1234";
 
-    public static Connection getConnection() {
-        return getConnection(hostName, dbName, user, password);
-    }
-
     /*
-    * Generic getConnection method
-    */
+     * Generic getConnection method
+     */
     public static Connection getConnection(String hostName, String dbName, String user, String password) {
         try {
             String url = "jdbc:mysql://" + hostName + ":3306/" + dbName;
@@ -24,5 +18,29 @@ public class DatabaseUtil {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static Connection getConnection() {
+        return getConnection(hostName, dbName, user, password);
+    }
+
+    /**
+     *
+     * @param query sql query where '?' are used as placeholders for params
+     * @param params value for the '?'s in query string
+     * @return ResultSet from query, returns null if none was queried
+     */
+    public static ResultSet query(String query, String... params) {
+        try (Connection connection = DatabaseUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            int paramIndex = 1;
+            for (String param : params) {
+                statement.setString(paramIndex++, param);
+            }
+            return statement.executeQuery();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }
--- src/main/java/tesda/tcsdi/simplepos/model/dal/ProductDB.java
+++ src/main/java/tesda/tcsdi/simplepos/model/dal/ProductDB.java
@@ -2,42 +2,24 @@ package tesda.tcsdi.simplepos.model.dal;
 
 import tesda.tcsdi.simplepos.model.Product;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class ProductDB extends InventoryDB {
-
-
-    private static ResultSet queryByColumn(String col, String q) {
-        String query = "SELECT * FROM products LEFT JOIN categories WHERE ? = ?";
-        try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, col);
-            statement.setString(2, q);
-            return statement.executeQuery();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }
-
-    private static ResultSet queryByColumn(String col, int q) {
-        return queryByColumn(col, Integer.toString(q));
-    }
+public class ProductDB extends DatabaseUtil{
 
     private static Product resultSetToProduct(ResultSet rs) {
         try (rs) {
             // Checks if rs is empty
             if (!rs.next()) return null;
             Product product = new Product();
-            product.setID(rs.getInt("id"));
+            product.setId(rs.getInt("id"));
             product.setName(rs.getString("name"));
             product.setBarcode(rs.getString("barcode"));
-            product.setSuggestedPrice(rs.getDouble("suggested_price"));
+            product.setPrice(rs.getDouble("price"));
+            product.setPrice(rs.getInt("quantity"));
             product.setCategory(rs.getString("category"));
+            product.setSupplierId(rs.getInt("supplier_id"));
             return product;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -45,50 +27,53 @@ public class ProductDB extends InventoryDB {
         }
     }
 
-    private static Product getByInt(String dbColumn, int some_id) {
-        try (ResultSet rs = queryByColumn(dbColumn, some_id)) {
-            return resultSetToProduct(rs);
+    public static Product getByID(int id) {
+        String queryStatement = "SELECT * FROM products LEFT JOIN categories WHERE ? = ?";
+        try (ResultSet rs = query(queryStatement, "id", Integer.toString(id)) ) {
+            if (rs != null) return resultSetToProduct(rs);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return null;
     }
 
-    public static Product getByID(int id) {
-        Product product = getByInt("id", id);
-        if (product != null) product.setInventoryID(getOldestStock(product.getID()));
-        return product;
-    }
-
-    public static Product getByBarcode(int barcode) {
-        Product product = getByInt("id", barcode);
-        if (product != null) product.setInventoryID(getOldestStock(product.getID()));
-        return product;
-    }
-
-    // can be migrated to another class InventoryDB
-    public static Product getByInventoryID(int inventoryID) {
-        Product product = getByInt("inventory_id", inventoryID);
-        if (product != null) product.setInventoryID(inventoryID);
-        return product;
+    public static Product getByBarcode(String barcode) {
+        String queryStatement = "SELECT * FROM products LEFT JOIN categories WHERE ? = ?";
+        try (ResultSet rs = query(queryStatement, "barcode", barcode)) {
+            if (rs != null) return resultSetToProduct(rs);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 
     public static ArrayList<Product> searchByName(String name) {
-            String query = "SELECT * FROM products LEFT JOIN categories WHERE name LIKE %?%";
-            try (Connection connection = DatabaseUtil.getConnection();
-                 PreparedStatement statement = connection.prepareStatement(query)) {
-                statement.setString(1, name);
-                try (ResultSet rs = statement.executeQuery()) {
-                    ArrayList<Product> products = new ArrayList<>();
-                    while (rs.next()) {
-                        products.add(resultSetToProduct(rs));
-                    }
-                    return products;
-                }
+        return searchByName(name, "name");
+    }
 
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
+    public static ArrayList<Product> searchByName(String name, String orderBy) {
+        return searchByName(name, orderBy, true);
+    }
+
+    public static ArrayList<Product> searchByName(String name, String orderBy, boolean descending) {
+        String desc = descending ? "DESC" : "";
+        String queryStatement = STR."SELECT * FROM products LEFT JOIN categories WHERE name LIKE %?% ORDER BY ? \{desc}";
+        try (ResultSet rs = query(queryStatement, name, orderBy)) {
+            ArrayList<Product> products = new ArrayList<>();
+            if (rs != null) {
+                while (rs.next()) {
+                    products.add(resultSetToProduct(rs));
+                }
+                return products;
             }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
         return null;
+    }
+
+    public static Product save(Product product) {
+        // TODO: implement save productDB method
+        return product;
     }
 }
