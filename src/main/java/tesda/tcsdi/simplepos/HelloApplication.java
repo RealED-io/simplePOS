dd86129e4204670392f1a7202a08a5ca9ebfb268
ed realuyo <ed.realuyo@gmail.com>
2024-11-28 23:00:33 +0800

    Modified Main class to start with Cashier view as test

--- src/main/java/tesda/tcsdi/simplepos/HelloApplication.java
+++ src/main/java/tesda/tcsdi/simplepos/HelloApplication.java
@@ -1,4 +1,5 @@
 package tesda.tcsdi.simplepos;
+import tesda.tcsdi.simplepos.model.Cart;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,8 +11,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("cashier-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setMinWidth(1280);
+        stage.setMinHeight(720);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
