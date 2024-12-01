71bbf39de46b9ad59abf19c93f5b6fb381170622
ed realuyo <ed.realuyo@gmail.com>
2024-12-01 23:36:28 +0800

    Added invoiceDB

---
+++ src/main/java/tesda/tcsdi/simplepos/model/dal/InvoiceDB.java
@@ -0,0 +1,63 @@
+package tesda.tcsdi.simplepos.model.dal;
+
+import tesda.tcsdi.simplepos.model.Invoice;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class InvoiceDB extends DatabaseUtil {
+
+    private Invoice resultSetToInvoice(ResultSet rs) {
+        try {
+            Invoice invoice = new Invoice();
+            invoice.setId(rs.getInt("id"));
+            invoice.setEmployeeId(rs.getInt("employee_id"));
+            if (invoice.getEmployeeId() != 0) {
+                EmployeeDB employee = new EmployeeDB();
+                String employeeName = employee.getById(invoice.getEmployeeId()).getName();
+                invoice.setEmployee(employeeName);
+            } else {
+                invoice.setEmployee(null);
+            }
+            invoice.setTotalAmount(rs.getDouble("total_amount"));
+            invoice.setIssueDate(rs.getDate("issue_date"));
+            return invoice;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Invoice getById(int id) {
+        String queryStatement = "SELECT * FROM invoices WHERE id = ?";
+        Invoice invoice = null;
+        try {
+            ResultSet rs = query(queryStatement, String.valueOf(id));
+            if (rs.next()) invoice = resultSetToInvoice(rs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+        return invoice;
+    }
+
+    public ArrayList<Invoice> getAll() {
+        String queryStatement = "SELECT * FROM invoices";
+        ArrayList<Invoice> invoices = null;
+        try (ResultSet rs = query(queryStatement)) {
+            if (rs != null) {
+                invoices = new ArrayList<>();
+                while (rs.next()) invoices.add(resultSetToInvoice(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+        return invoices;
+    }
+
+
+}
