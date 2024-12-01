7c16d7d8e2cfa538a14d47376ed73916674897ad
ed realuyo <ed.realuyo@gmail.com>
2024-12-01 16:43:59 +0800

    abstracted the process in start method

--- src/main/java/tesda/tcsdi/simplepos/Main.java
+++ src/main/java/tesda/tcsdi/simplepos/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
         ViewUtil.switchToLoginUI(stage);
     }
 
