add6fb15e9e4730f8ff3d40e787938e6ed5f4b60
ed realuyo <ed.realuyo@gmail.com>
2024-12-01 13:41:57 +0800

    import cleanup

--- src/main/java/tesda/tcsdi/simplepos/LoginController.java
+++ src/main/java/tesda/tcsdi/simplepos/LoginController.java
@@ -2,16 +2,12 @@ package tesda.tcsdi.simplepos;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import tesda.tcsdi.simplepos.model.Employee;
@@ -20,7 +16,6 @@ import tesda.tcsdi.simplepos.model.dal.EmployeeDB;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
