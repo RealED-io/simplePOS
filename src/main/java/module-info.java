1f48f96913f2a40f78bdae0ad06f893168b22d9b
ed realuyo <ed.realuyo@gmail.com>
2024-12-02 00:23:56 +0800

    updated module-info.java to support latest build

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module tesda.tcsdi.simplepos {
     requires java.sql;
     requires javafx.fxml;
     requires javafx.controls;
+    requires java.xml.crypto;
 
     opens tesda.tcsdi.simplepos to javafx.fxml;
     opens tesda.tcsdi.simplepos.model to javafx.fxml;
