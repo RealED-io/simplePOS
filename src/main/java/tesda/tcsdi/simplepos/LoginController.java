126257bdc069c7cbf641537f952314eb92917924
ed realuyo <ed.realuyo@gmail.com>
2024-12-02 04:45:57 +0800

    added passing of employee to controller

--- src/main/java/tesda/tcsdi/simplepos/LoginController.java
+++ src/main/java/tesda/tcsdi/simplepos/LoginController.java
@@ -2,7 +2,10 @@ package tesda.tcsdi.simplepos;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -45,8 +48,20 @@ public class LoginController implements Initializable {
             if (!employee.getPassword().equals(passwordField.getText())) break;
             loginMessage.setText("login successful");
             Stage stage = ViewUtil.getStage(event);
-            if (employee.getRole().equals("cashier")) ViewUtil.switchToCashierUI(stage);
+            if (employee.getRole().equals("cashier")) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("cashier-view.fxml"));
+                Parent root = loader.load();
+
+                CashierController cashierController = loader.getController();
+                cashierController.passEmployeeToController(employee);
+
+                ViewUtil.getStage(event).setScene(new Scene(root));
+                stage.setMinWidth(1280);
+                stage.setHeight(720);
+                stage.show();
+            }
             else if (employee.getRole().equals("manager")) ViewUtil.switchToManagerUI(stage);
+
             return;
         }
         loginMessage.setText("login failed");
