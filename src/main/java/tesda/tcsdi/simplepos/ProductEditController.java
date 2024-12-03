b7c426ac74de22d2e42540bd452df2d7689961b3
ed realuyo <ed.realuyo@gmail.com>
2024-12-03 22:19:29 +0800

    added input validation and fixed update, create and delete to db functions

--- src/main/java/tesda/tcsdi/simplepos/ProductEditController.java
+++ src/main/java/tesda/tcsdi/simplepos/ProductEditController.java
@@ -6,21 +6,21 @@ import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
+import tesda.tcsdi.simplepos.model.Category;
 import tesda.tcsdi.simplepos.model.Product;
+import tesda.tcsdi.simplepos.model.Supplier;
+import tesda.tcsdi.simplepos.model.dal.CategoryDB;
 import tesda.tcsdi.simplepos.model.dal.ProductDB;
+import tesda.tcsdi.simplepos.model.dal.SupplierDB;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class ProductEditController implements Initializable{
 
@@ -78,46 +78,127 @@ public class ProductEditController implements Initializable{
     @FXML
     private ComboBox<String> supplierField;
 
-    @FXML
-    void createProduct(ActionEvent event) {
-
-
-    }
+    private Product selectedItem;
+    private final ProductDB productFactory = new ProductDB();
+    private final CategoryDB categoryFactory = new CategoryDB();
+    private final SupplierDB supplierFactory = new SupplierDB();
+    private ObservableList<Product> productList;
+    private FilteredList<Product> filteredData;
+    private ObservableList<String> categoryOptions;
+    private ObservableList<String> supplierOptions;
 
     @FXML
     void newProduct(ActionEvent event) {
+        if (!areValidTextFields()) return;
+        Product product = textFieldToProduct();
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Delete confirmation");
-        alert.setContentText("Are you sure you want to delete \"" + selectedItem.getName() + "\"?");
+        alert.setTitle("New item confirmation");
+        alert.setContentText("Are you sure you want to add \"" + product.getName() + "\" as a new item?");
         Optional<ButtonType> buttonType = alert.showAndWait();
         if(buttonType.isPresent() && buttonType.get().equals(ButtonType.OK)) {
-            ProductDB productFactory = new ProductDB();
-            productFactory.delete(selectedItem);
-            filteredData.getSource().remove(selectedItem);
-            selectedItem = null;
+            productFactory.create(product);
+            productTableInit();
         }
     }
 
     @FXML
-    void updateProduct(ActionEvent event) throws IOException {
-//        ViewUtil.getStage(event).getScene().getRoot();
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("product-dialog.fxml"));
-//        Parent root = loader.load();
-//        ProductDialogController controller = loader.getController();
-//        controller.passProductToController(selectedItem);
-//        Stage stage = ViewUtil.getStage(event);
-//        stage.setScene(new Scene(root));
-//        stage.show();
+    void updateProduct(ActionEvent event) {
+        if (!areValidTextFields()) return;
+        Product product = textFieldToProduct();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Update item confirmation");
+        alert.setContentText("Are you sure you want to update \"" + product.getName() + "\"?");
+        Optional<ButtonType> buttonType = alert.showAndWait();
+        if(buttonType.isPresent() && buttonType.get().equals(ButtonType.OK)) {
+            productFactory.update(product);
+            productTableInit();
+        }
     }
 
-    private Product selectedItem;
-    private final ProductDB productFactory = new ProductDB();
-    private FilteredList<Product> filteredData;
+    @FXML
+    void deleteProduct(ActionEvent event) {
+        if (!areValidTextFields()) return;
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("New item confirmation");
+        alert.setContentText("Are you sure you want to add \"" + selectedItem.getName() + "\" as a new item?");
+        Optional<ButtonType> buttonType = alert.showAndWait();
+        if(buttonType.isPresent() && buttonType.get().equals(ButtonType.OK)) {
+            if (selectedItem.getId() != 0) productFactory.delete(selectedItem);
+            productTableInit();
+        }
+    }
 
+    @FXML
+    void clearTextFields(ActionEvent event) {
+        idField.clear();
+        nameField.clear();
+        barcodeField.clear();
+        priceField.clear();
+        quantityField.clear();
+        quantityTypeField.getSelectionModel().clearSelection();
+        categoryField.getSelectionModel().clearSelection();
+        supplierField.getSelectionModel().clearSelection();
+    }
+
+    @FXML
+    void selectProductFromTable(MouseEvent event) {
+        selectedItem = productTable.getSelectionModel().getSelectedItem();
+        if(selectedItem == null) return;
+        idField.setText(String.valueOf(selectedItem.getId()));
+        nameField.setText(selectedItem.getName());
+        barcodeField.setText(selectedItem.getBarcode());
+        priceField.setText(String.valueOf(selectedItem.getPrice()));
+        quantityField.setText(String.valueOf(selectedItem.getInventoryQuantity()));
+        quantityTypeField.setValue(selectedItem.getQuantityType());
+        categoryField.setValue(selectedItem.getCategory());
+        supplierField.setValue(selectedItem.getSupplier());
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ObservableList<Product> productList = FXCollections.observableArrayList(productFactory.getAll());
+        productTableInit();
+        quantityTypeComboBoxInit();
+        categoryComboBoxInit();
+        supplierComboBoxInit();
+
+        nameField.setOnKeyTyped(event -> areValidTextFields());
+        barcodeField.setOnKeyTyped(event -> areValidTextFields());
+        priceField.setOnKeyTyped(event -> areValidTextFields());
+        quantityField.setOnKeyTyped(event -> areValidTextFields());
+        quantityTypeField.setOnAction(event -> areValidTextFields());
+        quantityField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) quantityField.setText(oldValue);
+        });
+        priceField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("-?\\d*(\\.\\d*)?")) priceField.setText(oldValue);
+        });
+        }
+
+    private void supplierComboBoxInit() {
+        supplierOptions = FXCollections.observableArrayList(
+                supplierFactory.getAll().stream()
+                        .map(Supplier::getName)
+                        .toList()
+        );
+        supplierField.setItems(supplierOptions);
+    }
+
+    private void quantityTypeComboBoxInit() {
+        ObservableList<String> quantityTypeOptions = FXCollections.observableArrayList("counted", "weighted (kg)");
+        quantityTypeField.setItems(quantityTypeOptions);
+    }
+
+    private void categoryComboBoxInit() {
+        categoryOptions = FXCollections.observableArrayList(
+                categoryFactory.getAll().stream()
+                        .map(Category::getName)
+                        .collect(Collectors.toList())
+        );
+        categoryField.setItems(categoryOptions);
+    }
+
+    private void productTableInit() {
+        productList = FXCollections.observableArrayList(productFactory.getAll());
         // Product CellValueFactory
         idCol.setCellValueFactory(new PropertyValueFactory<Product, Integer>("id"));
         barcodeCol.setCellValueFactory(new PropertyValueFactory<Product, String>("barcode"));
@@ -128,29 +209,84 @@ public class ProductEditController implements Initializable{
         categoryCol.setCellValueFactory(new PropertyValueFactory<Product, String>("category"));
         supplierCol.setCellValueFactory(new PropertyValueFactory<Product, String>("supplier"));
 
-        // For search, filteredData contains data that does not fit filter
+        // For search
         filteredData = new FilteredList<>(productList, b -> true);
         searchField.textProperty().addListener((observable, oldValue, newValue) -> {
             filteredData.setPredicate(product -> {
-                // If filter text is empty, display all persons.
                 if (newValue == null || newValue.isEmpty()) return true;
-
                 String lowerCaseFilter = newValue.toLowerCase();
-                if (product.getName().toLowerCase().contains(lowerCaseFilter)) return true; // Filter matches first name.
-                else if (product.getBarcode().toLowerCase().contains(lowerCaseFilter)) return true; // Filter matches last name.
-                else return false; // Does not match.
+                if (product.getName().toLowerCase().contains(lowerCaseFilter)) return true;
+                else if (product.getBarcode().toLowerCase().contains(lowerCaseFilter)) return true;
+                else return false;
             });
         });
-
-
         SortedList<Product> sortedData = new SortedList<>(filteredData);
         sortedData.comparatorProperty().bind(productTable.comparatorProperty());
         productTable.setItems(sortedData);
     }
 
+    private Product textFieldToProduct() {
+        Product product = new Product();
+        if(!idField.getText().isBlank()) product.setId(Integer.parseInt(idField.getText()));
+        product.setBarcode(barcodeField.getText());
+        product.setName(nameField.getText());
+        product.setPrice(Double.parseDouble(priceField.getText()));
+        product.setInventoryQuantity(Integer.parseInt(quantityField.getText()));
+        product.setQuantityType(quantityTypeField.getValue());
+        product.setCategory(categoryField.getValue());
+        if(product.getCategory() == null) product.setCategoryId(0);
+        else if (!categoryOptions.contains(product.getCategory())) {
+            Category newCategory = new Category();
+            newCategory.setName(product.getCategory());
+            newCategory = categoryFactory.create(newCategory);
+            product.setCategoryId(newCategory.getId());
+            categoryOptions.add(newCategory.getName());
+        } else product.setCategoryId(categoryFactory.getByName(product.getCategory()).getId());
 
-    @FXML
-    public void onItemClicked(MouseEvent mouseEvent) {
-        selectedItem = productTable.getSelectionModel().getSelectedItem();
+        product.setSupplier(supplierField.getValue());
+        if(product.getSupplier() == null) product.setSupplierId(0);
+        else if(!supplierOptions.contains(product.getSupplier())) {
+            Supplier newSupplier = new Supplier();
+            newSupplier.setName(product.getSupplier());
+            newSupplier = supplierFactory.create(newSupplier);
+            product.setSupplierId(newSupplier.getId());
+            supplierOptions.add(newSupplier.getName());
+        } else product.setSupplierId(supplierFactory.getByName(product.getSupplier()).getId());
+        return product;
+    }
+
+    private boolean areValidTextFields() {
+        String warningCSS = "-fx-border-color: red; -fx-background-color: #ffe6e6;";
+        boolean valid = true;
+        if(nameField.getText() == null || nameField.getText().isBlank()) {
+            valid = false;
+            nameField.setStyle(warningCSS);
+        } else nameField.setStyle("");
+        if(barcodeField.getText() == null || barcodeField.getText().isBlank() || barcodeField.getText().length() > 15 ||
+                (idField.getText().isBlank() && productList.stream().map(Product::getBarcode).toList().contains(barcodeField.getText()))) {
+            valid = false;
+            barcodeField.setStyle(warningCSS);
+        } else barcodeField.setStyle("");
+        try {
+            Double price = Double.valueOf(priceField.getText());
+            priceField.setStyle("");
+        } catch (Exception e) {
+            valid = false;
+            priceField.setStyle(warningCSS);
+        }
+        try {
+            int quantity = Integer.valueOf(quantityField.getText());
+            quantityField.setStyle("");
+        } catch (Exception e) {
+            valid = false;
+            quantityField.setStyle(warningCSS);
+        }
+        if(quantityTypeField.getSelectionModel().isEmpty()) {
+            valid = false;
+            quantityTypeField.setStyle(warningCSS);
+        } else {
+            quantityTypeField.setStyle("");
+        }
+        return valid;
     }
 }
