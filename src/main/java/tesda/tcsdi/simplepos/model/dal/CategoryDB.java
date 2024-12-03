28edca8c3ff0140b4407f31493a108a4aceb4956
ed realuyo <ed.realuyo@gmail.com>
2024-12-03 09:30:14 +0800

    added create, update, delete and getById

--- src/main/java/tesda/tcsdi/simplepos/model/dal/CategoryDB.java
+++ src/main/java/tesda/tcsdi/simplepos/model/dal/CategoryDB.java
@@ -1,6 +1,7 @@
 package tesda.tcsdi.simplepos.model.dal;
 
 import tesda.tcsdi.simplepos.model.Category;
+import tesda.tcsdi.simplepos.model.Product;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -19,6 +20,20 @@ public class CategoryDB extends DatabaseUtil{
         }
     }
 
+    public Category getById(int id) {
+        String queryStatement = "SELECT * FROM categories WHERE id = ?";
+        Category category = null;
+        try {
+            ResultSet rs = query(queryStatement, String.valueOf(id));
+            if (rs.next()) category = resultSetToCategory(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+        return category;
+    }
+
     public ArrayList<Category> getAll() {
         String queryStatement = "SELECT * FROM categories";
         ArrayList<Category> categories = null;
@@ -33,5 +48,26 @@ public class CategoryDB extends DatabaseUtil{
             closeConnection();
         }
         return categories;
+    }
+
+    public Category create(Category category) {
+        String queryStatement = "INSERT INTO categories " +
+                "(name) " +
+                "VALUES (?)";
+        int id = save(queryStatement, category.getName());
+        category.setId(id);
+        closeConnection();
+        return category;
+    }
+
+    public void update(Category category) {
+        String queryStatement = "UPDATE categories SET name = ? WHERE id = ?";
+        save(queryStatement, category.getName(), String.valueOf(category.getId()));
+        closeConnection();
+    }
+
+    public void delete(Category category) {
+        String queryStatement = "DELETE FROM categories WHERE id = ?";
+        save(queryStatement, String.valueOf(category.getId()));
     }
 }
